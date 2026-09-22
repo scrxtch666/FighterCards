361e2126fec12b6aea4be452393aa909675fd516
scrxtch <peetr.svoboda@seznam.cz>
2026-09-22 17:41:58 +0200

    Tailwindcss + refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,28 @@ import { defineConfig } from 'astro/config';
 
 import db from "@astrojs/db";
 
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from 'url';
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+       watch: {
+        usePolling: true
+      },
+      fs: {
+        // Povolí přístup k souborům pouze uvnitř projektu
+        allow: [fileURLToPath(new URL('.', import.meta.url))],
+      },
+      watch: {
+        // Ignoruje sledování čehokoliv mimo váš projekt
+        ignored: ['C:/**'],
+      }
+    },
+  },
 });
--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.post('/save-data', (req, res) => {
-    const { name, email } = req.body;
+    const { name, age, weight, height, bio } = req.body;
 
-    // Přečtěte existující soubor
     fs.readFile('public/data.js', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Chyba při čtení souboru');
         }
 
-        // Přidejte nová data do existujícího obsahu
-        const newData = `\nconst user = { name: "${name}", email: "${email}" };`;
+        const newData = `\nconst user = { name: "${name}", age: "${age}", weight: "${weight}", height: "${height}", bio: "${bio}" };`;
         const updatedData = data + newData;
 
-        // Uložte aktualizovaný obsah zpět do souboru
         fs.writeFile('public/data.js', updatedData, 'utf8', (err) => {
             if (err) {
                 console.error(err);
